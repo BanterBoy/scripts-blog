9f18b2d5822aeee1f5d8cee7a545f96812d6c6b4
Luke Leigh <luke.leigh@gmail.com>
2020-10-16 02:13:26 +0100

    Update Move-DisabledUsers.ps1

--- PowerShell/scripts/activeDirectory/Move-DisabledUsers.ps1
+++ PowerShell/scripts/activeDirectory/Move-DisabledUsers.ps1
@@ -1,9 +1,9 @@
 foreach ($DisabledUser in $DisabledUsers) {
     try {
-        Move-ADObject -Identity $DisabledUser.DistinguishedName -TargetPath "OU=Disabled Accounts,OU=Ventrica,DC=ventrica,DC=local" -WhatIf
+        Move-ADObject -Identity $DisabledUser.DistinguishedName -TargetPath "OU=Disabled Accounts,OU=Example,DC=Example,DC=local" -WhatIf
     }
     catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
         Write-Warning "Move-ADObject : Directory object not found $DisabledUser.Name" -OutVariable $Global:MoveErrors
     }
-    Out-File -InputObject $MoveErrors -FilePath "C:\GitRepos\Ventrica\UpdatePlan\ADTidy\RemovedUsers\Move-DisabledUsersError.txt" -Append
+    Out-File -InputObject $MoveErrors -FilePath "C:\RemovedUsers\Move-DisabledUsersError.txt" -Append
 }
