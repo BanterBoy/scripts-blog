8c15d1e307f986020afc2cb8f802ee9748fe33a8
Luke Leigh <luke.leigh@gmail.com>
2021-01-03 05:35:03 +0000

    Update Start-ADSyncCycle.ps1

--- PowerShell/scripts/activeDirectory/Start-ADSyncCycle.ps1
+++ PowerShell/scripts/activeDirectory/Start-ADSyncCycle.ps1
@@ -1,10 +1,12 @@
-Import-Module ADSync
+Import-Module –Name ADSync
+
+# Import-Module –Name "C:\Program Files\Microsoft Azure AD Sync\Bin\ADSync" -Verbose
 
 Get-ADSyncScheduler
 
 # To force a full sync from Windows PowerShell
 
-Start-ADSyncSyncCycle -PolicyType Initial
+Start-ADSyncSyncCycle -PolicyType Full
 
 # To force a delta sync:
 
