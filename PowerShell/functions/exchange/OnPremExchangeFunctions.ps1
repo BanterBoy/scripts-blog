87703deb76b18ac1a6106dd417aca5076279ad98
Luke Leigh <luke.leigh@gmail.com>
2021-01-03 05:35:20 +0000

    Update OnPremExchangeFunctions.ps1

--- PowerShell/functions/exchange/OnPremExchangeFunctions.ps1
+++ PowerShell/functions/exchange/OnPremExchangeFunctions.ps1
@@ -1,20 +1,23 @@
 function New-OnPremExchangeSession {
     param (
         [Parameter(ValueFromPipeline = $True,
-        HelpMessage = "Enter preferred Exchange Server")]
-		[ValidateSet('MAIL01','MAIL02') ]
+            HelpMessage = "Enter preferred Exchange Server")]
+        [ValidateSet('MAIL01', 'MAIL02') ]
         [string[]]$ComputerName
     )
-    switch($ComputerName){
+    switch ($ComputerName) {
         MAIL01 {
+            $Creds = Get-Credential
             $OnPremSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://INTERNAL-EXCHANGE-URI/PowerShell/ -Authentication Kerberos -Credential $Creds
             Import-PSSession $OnPremSession -DisableNameChecking
         }
         MAIL02 {
+            $Creds = Get-Credential
             $OnPremSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://INTERNAL-EXCHANGE-URI/PowerShell/ -Authentication Kerberos -Credential $Creds
             Import-PSSession $OnPremSession -DisableNameChecking
         }
         default {
+            $Creds = Get-Credential
             $OnPremSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://INTERNAL-EXCHANGE-URI/PowerShell/ -Authentication Kerberos -Credential $Creds
             Import-PSSession $OnPremSession -DisableNameChecking
         }
