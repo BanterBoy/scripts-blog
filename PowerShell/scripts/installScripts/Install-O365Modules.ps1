ec525386268a65db32e2dada64764c8d215b8cf2
Luke Leigh <luke.leigh@gmail.com>
2021-01-03 05:34:57 +0000

    Update Install-O365Modules.ps1

--- PowerShell/scripts/installScripts/Install-O365Modules.ps1
+++ PowerShell/scripts/installScripts/Install-O365Modules.ps1
@@ -2,14 +2,13 @@ $execpol = Get-ExecutionPolicy -List
 foreach ($exec in $execpol) { Set-ExecutionPolicy -ExecutionPolicy Unrestricted }
 
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-
-Install-Module -Name AADRM -Scope AllUsers
-Install-Module -Name AzureAD -Scope AllUsers
-Install-Module -Name AzureADPreview -Scope AllUsers -AllowClobber
-Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Scope AllUsers
-Install-Module -Name MicrosoftTeams -Scope AllUsers
-Install-Module -Name MSOnline -Scope AllUsers
-Install-Module -Name SharePointPnPPowerShellOnline -Scope AllUsers
-
+Install-Module -Name AADRM -Scope AllUsers -Force
+Install-Module -Name AzureAD -Scope AllUsers -Force
+Install-Module -Name AzureADPreview -Scope AllUsers -Force -AllowClobber
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Scope AllUsers -Force
+Install-Module -Name MicrosoftTeams -Scope AllUsers -Force
+Install-Module -Name MSOnline -Scope AllUsers -Force
+Install-Module -Name SharePointPnPPowerShellOnline -Scope AllUsers -Force
+Install-Module -Name ExchangeOnlineManagement -Scope AllUsers -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
 
