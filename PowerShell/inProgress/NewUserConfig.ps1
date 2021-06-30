4d16790bf804a0808a958744c161a71ddcd70f85
Luke Leigh <luke.leigh@gmail.com>
2021-06-30 14:07:07 +0100

    Create NewUserConfig.ps1

---
+++ PowerShell/inProgress/NewUserConfig.ps1
@@ -0,0 +1,23 @@
+function Get-UserConfig {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ParameterSetName = 'ParameterSet1')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [OutputType([string])]
+        [ArgumentCompleter( {
+                $baseOU = (Get-ADDomain).DistinguishedName
+                $OUs = Get-ADOrganizationalUnit -Filter * -SearchScope SubTree -SearchBase $baseOU | Sort-Object -Property DistinguishedName
+                foreach ($OU in $OUs) { 
+                    ($OU).DistinguishedName
+                }
+            } ) ]
+        [string]
+        $SelectOU
+    )
+}
+
+
