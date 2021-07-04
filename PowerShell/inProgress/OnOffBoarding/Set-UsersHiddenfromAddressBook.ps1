30975c2c77aef74d0af5bd32686d6990da8482e0
Luke Leigh <luke.leigh@gmail.com>
2021-07-04 18:33:50 +0100

    Update Set-UsersHiddenfromAddressBook.ps1

--- PowerShell/inProgress/OnOffBoarding/Set-UsersHiddenfromAddressBook.ps1
+++ PowerShell/inProgress/OnOffBoarding/Set-UsersHiddenfromAddressBook.ps1
@@ -1,22 +1,12 @@
-$DisabledUsers = Get-ADUser -Filter { Name -like '*' } -SearchBase "OU=Users,OU=Disabled Accounts,DC=ventrica,DC=local" |
-
-    Where-Object { $_.Enabled -eq $false }
-
-foreach($User in $DisabledUsers) {
-
+$DisabledUsers = Get-ADUser -Filter { Name -like '*' } -SearchBase "OU=Users,OU=Disabled Accounts,DC=domain,DC=local" |
+Where-Object { $_.Enabled -eq $false }
+foreach ($User in $DisabledUsers) {
     try {
-
         Get-Mailbox -Identity $User.UserPrincipalName -ErrorAction Stop |
-
         Set-Mailbox -HiddenFromAddressListsEnabled $true -ErrorAction Stop -Whatif
-
-        }
-
-    catch {
-
-        Write-Verbose -Message $_.Exception.Message -Verbose
-
     }
-
+    catch {
+        Write-Verbose -Message $_.Exception.Message -Verbose
+    }
 }
 
