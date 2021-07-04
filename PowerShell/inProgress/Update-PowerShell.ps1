863f8195b9e271fc7e42965db2c15df803cb9041
Luke Leigh <luke.leigh@gmail.com>
2021-07-04 04:08:27 +0100

    Update Update-PowerShell.ps1

--- PowerShell/inProgress/Update-PowerShell.ps1
+++ PowerShell/inProgress/Update-PowerShell.ps1
@@ -1,3 +1,37 @@
 function Update-PowerShell {
-    Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI"
+
+    # Test if PowerShellForGitHub module is installed and if not, install module.
+    $Module = "PowerShellForGitHub"
+
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+    if ( Get-Module -Name $Module ) {
+        Import-Module -Name $Module
+        Write-Warning "Module Import - Imported $Module"
+    }
+
+    else {
+        Write-Warning "Installing $Module"
+        $execpol = Get-ExecutionPolicy -List
+        if ( $execpol -ne 'Unrestricted' ) {
+            Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+        }
+        Install-Module -Name $Module -Scope CurrentUser
+    }
+
+    Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
+
+    # Test current version installed and update if not latest release
+    if ($GitRelease -like "*" + $LocalVersion) {
+        Write-Warning "Up-to-date!"
+    }
+
+    else {
+        $Protocols = [System.Net.SecurityProtocolType]'Tls12'
+        [System.Net.ServicePointManager]::SecurityProtocol = $Protocols
+        Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI"
+    }
+
 }
+
+Update-PowerShell
