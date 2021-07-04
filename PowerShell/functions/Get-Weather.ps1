53bfdcd024324fd76e88a7206dd25d125e1522cf
Luke Leigh <luke.leigh@gmail.com>
2021-07-04 18:33:31 +0100

    Update Get-Weather.ps1

--- PowerShell/functions/Get-Weather.ps1
+++ PowerShell/functions/Get-Weather.ps1
@@ -1,10 +1,20 @@
-param 
-(
-    [Parameter(Mandatory)]$City
-)
+function Get-Weather {
+    <#
+	.SYNOPSIS
+	Get the Patch Tuesday of a month
+	.PARAMETER City
+	The city to check
+	.EXAMPLE
+    Get-Weather -City 'Southend-on-Sea'
+	#>
 
-$weather = Invoke-WebRequest -Uri "http://wttr.in/$City"
-$text = $weather.ParsedHtml.body.outerText
-$text
-
-# Example : Get-Weather.ps1 -City 'Southend-on-Sea'
+    param 
+    (
+        [Parameter(Mandatory)]
+        $City
+    )
+    $Protocols = [Net.SecurityProtocolType]'Tls12'
+    [Net.ServicePointManager]::SecurityProtocol = $Protocols
+    $Weather = Invoke-RestMethod -Uri "http://wttr.in/$City"
+    $Weather
+}
