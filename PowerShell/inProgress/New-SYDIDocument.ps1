224a62ecd8aaaea629dc2dea50b0518f919e477b
Luke Leigh <luke.leigh@gmail.com>
2021-07-25 22:18:51 +0100

    Delete New-SYDIDocument.ps1

--- PowerShell/inProgress/New-SYDIDocument.ps1
+++
@@ -1,4 +0,0 @@
-http://sydiproject.com/
-
-cscript.exe 'D:\LazyWinAdmin\scripts\Sydi-Server.vbs' -wabefghipPqrsSu -racdklp -ew -f10 -d -o'D:\ServerDocumentation\WEB03.docx' -t'WEB03' -u'lukeleigh.adm' -p'1ukE1Eigh.3742'
-
