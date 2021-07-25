ca80e44dad03ccdf7bfd8a1c36775f28c0677f97
Luke Leigh <luke.leigh@gmail.com>
2021-07-25 22:19:22 +0100

    Update Teams.ps1

--- PowerShell/inProgress/Teams.ps1
+++ PowerShell/inProgress/Teams.ps1
@@ -9,18 +9,18 @@ $ErrorAction = "SilentlyContinue"
 $allteams = Get-Team | Where-Object { $_.DisplayName -eq 'IT' }
 $object = @()
  
-foreach ($t in $allteams) {
+foreach ($team in $allteams) {
  
-    $members = Get-TeamUser -GroupId $t.GroupId
+    $members = Get-TeamUser -GroupId $team.GroupId
  
-    $owner = Get-TeamUser -GroupId $t.GroupId -Role Owner
+    $owner = Get-TeamUser -GroupId $team.GroupId -Role Owner
  
-    $channels = Get-TeamChannel -GroupId $t.GroupId 
+    $channels = Get-TeamChannel -GroupId $team.GroupId 
  
     $object += New-Object -TypeName PSObject -Property ([ordered]@{
  
-            'Team'     = $t.DisplayName
-            'GroupId'  = $t.GroupId
+            'Team'     = $team.DisplayName
+            'GroupId'  = $team.GroupId
             'Owner'    = $owner.User
             'Members'  = $members.user -join "`r`n"
             'Channels' = $channels.displayname -join "`r`n"
