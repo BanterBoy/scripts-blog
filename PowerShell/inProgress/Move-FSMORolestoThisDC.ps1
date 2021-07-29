e6a0d533ffafebc760c18c0235751b38c6a6984f
Luke Leigh <luke.leigh@gmail.com>
2021-07-30 00:54:31 +0100

    Update Move-FSMORolestoThisDC.ps1

--- PowerShell/inProgress/Move-FSMORolestoThisDC.ps1
+++ PowerShell/inProgress/Move-FSMORolestoThisDC.ps1
@@ -1,1 +1,15 @@
-Move-ADDirectoryServerOperationMasterRole $env:COMPUTERNAME -OperationMasterRole 0, 1, 2, 3, 4
+
+function Move-FSMORolestoThisDC {
+    param (
+        [Parameter(
+            Mandatory = $false,
+            Position = 1,
+            ParameterSetName = "Default",
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName,
+            HelpMessage = "Enter the Domain Controller ComputerName")]
+        [string]
+        $ComputerName = $env:COMPUTERNAME
+    )
+    Move-ADDirectoryServerOperationMasterRole -Identity $ComputerName -OperationMasterRole 0, 1, 2, 3, 4
+}
