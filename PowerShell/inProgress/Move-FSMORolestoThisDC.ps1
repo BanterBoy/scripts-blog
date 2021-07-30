97532f5095c30cf4c9610dfedbe58ae77c03d771
Luke Leigh <luke.leigh@gmail.com>
2021-07-30 01:28:46 +0100

    Update Move-FSMORolestoThisDC.ps1

--- PowerShell/inProgress/Move-FSMORolestoThisDC.ps1
+++ PowerShell/inProgress/Move-FSMORolestoThisDC.ps1
@@ -1,6 +1,53 @@
 
 function Move-FSMORolestoThisDC {
-    param (
+    <#
+    
+        .SYNOPSIS
+        A function to reassign all of the Active Directory FSMO Roles to a specific computer.
+    
+        .DESCRIPTION
+        A function to reassign all of the Active Directory FSMO Roles to a specific computer.
+    
+        .PARAMETER ComputerName
+        Enter the Name of the Computer that you want to reassign the FSMO roles to.
+
+        .EXAMPLE
+        Move-FSMORolestoThisDC
+        
+        Move Operation Master Role
+        Do you want to move role 'PDCEmulator' to server 'DANTOOINE.domain.leigh-services.com' ?
+        [Y] Yes  [A] Yes to All  [N] No  [L] No to All  [S] Suspend  [?] Help (default is "Y"):
+        
+        When run without any parameters, the command uses the $env:COMPUTERNAME environment variable and attempt to move the roles.
+
+        .EXAMPLE
+        Move-FSMORolestoThisDC -ComputerName DANTOOINE -Verbose
+
+        Move Operation Master Role
+        Do you want to move role 'PDCEmulator' to server 'DANTOOINE.domain.leigh-services.com' ?
+        [Y] Yes  [A] Yes to All  [N] No  [L] No to All  [S] Suspend  [?] Help (default is "Y"):
+
+        When run with the ComputerName parameter, the command attempts to move the roles to the specified Domain Controller.
+        
+        .INPUTS
+        You can pipe objects to this perameter.
+    
+        - ComputerName [string]
+    
+        .NOTES
+        Author:     Luke Leigh
+        Website:    https://scripts.lukeleigh.com/
+        LinkedIn:   https://www.linkedin.com/in/lukeleigh/
+        GitHub:     https://github.com/BanterBoy/
+        GitHubGist: https://gist.github.com/BanterBoy
+    
+        .LINK
+        https://github.com/BanterBoy/scripts-blog
+        Move-ADDirectoryServerOperationMasterRole
+    
+    #>
+    [cmdletbinding(DefaultParameterSetName = "Default")]
+    Param(
         [Parameter(
             Mandatory = $false,
             Position = 1,
