ad0d1d96ecf4ca4f18260d94151c867fa69c34cd
Luke Leigh <luke.leigh@gmail.com>
2021-08-01 20:05:23 +0100

    Update Services.ps1

--- PowerShell/inProgress/Services.ps1
+++ PowerShell/inProgress/Services.ps1
@@ -29,24 +29,3 @@ function Get-DomainAdminUsers {
 
 Get-RemoteServiceAccount -ComputerName APP01 | Where-Object { ( $_.StartName -notmatch 'LocalSystem|^NT Authority' ) }
 
-
-$Computers = Get-ADComputer -Filter "Name -like '*'"
-ForEach ($Computer in $Computers) {
-    Get-CimInstance -ComputerName $Computer.Name -Class Win32_PingStatus -Filter ("Address='$_'") |
-    Select-Object -Property PSComputerName, Address, IPV4Address, ResponseTime, StatusCode
-}
-
-$Computers = 'DANTOOINE', 'AGAMAR', $ENV:COMPUTERNAME, 'localhost', 'bing.com', 'blog.lukeleigh.com', 'me.lukeleigh.com'
-ForEach ($Computer in $Computers) {
-    $Results = Get-CimInstance -ComputerName $Computer -Class Win32_PingStatus -Filter ("Address='$_'")
-    foreach ($Result in $Results) {
-        $Properties = @{
-            DNSName      = $Result.PSComputerName
-            Address      = $Result.Address
-            IPV4Address  = $Result.IPV4Address
-            ResponseTime = $Result.ResponseTime
-            StatusCode   = $Result.StatusCode
-        }
-        Write-Output -InputObject $Properties
-    }
-}
