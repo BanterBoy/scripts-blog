b7d03b7dd9916d1f8cda88177dfaf08b12449fd0
Luke Leigh <luke.leigh@gmail.com>
2021-08-13 17:56:05 +0100

    Update Test-ADUserCredentials.ps1

--- PowerShell/functions/activeDirectory/Test-ADUserCredentials.ps1
+++ PowerShell/functions/activeDirectory/Test-ADUserCredentials.ps1
@@ -18,3 +18,38 @@ function Test-ADUserCredentials {
     end {
     }
 }
+
+
+function Test-ADCredentials {
+    [CmdletBinding()]
+    param(
+        [pscredential]$Credential
+    )
+     
+    try {
+        Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+        if (!$Credential) {
+            $Credential = Get-Credential -EA Stop
+        }
+        if ($Credential.username.split("\").count -ne 2) {
+            throw "You haven't entered credentials in DOMAIN\USERNAME format. Given value : $($Credential.Username)"
+        }
+     
+        $DomainName = $Credential.username.Split("\")[0]
+        $UserName = $Credential.username.Split("\")[1]
+        $Password = $Credential.GetNetworkCredential().Password
+     
+        $PC = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Domain, $DomainName)
+        if ($PC.ValidateCredentials($UserName, $Password)) {
+            Write-Verbose "Credential validation successful for $($Credential.Username)"
+            return $True
+        }
+        else {
+            throw "Credential validation failed for $($Credential.Username)"
+        }
+    }
+    catch {
+        Write-Verbose "Error occurred while performing credential validation. $_"
+        return $False
+    }
+}
