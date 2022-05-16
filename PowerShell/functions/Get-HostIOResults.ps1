ba06ef4945a1b5d21fe3e2be6add07c94ddfcc24
Luke Leigh <luke.leigh@gmail.com>
2022-05-16 08:25:49 +0100

    Update docker-compose.yml

--- PowerShell/functions/Get-HostIOResults.ps1
+++ PowerShell/functions/Get-HostIOResults.ps1
@@ -1,6 +1,7 @@
 function Get-HostIOResults {
-    [CmdletBinding()]
-
+    [CmdletBinding(
+        SupportsShouldProcess = $true
+    )]
     param (
         [Parameter()]
         [string[]]
@@ -10,36 +11,33 @@ function Get-HostIOResults {
         [string[]]
         $apiKey
     )
-        
     begin {
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
         $siteURL = "https://host.io/api/full/"
         $accessKey = ("?token=" + "$ApiKey")
         $fullresults = Invoke-RestMethod -Method Get -Uri ($siteURL + $domainName + $accessKey)
     }
-    
     process {
-        try {
-            foreach ($result in $fullresults) {
-                $properties = @{
-                    "Domain"       = $fullresults.domain
-                    "WEB"          = $fullresults.web
-                    "DomainRecord" = $fullresults.dns.domain
-                    "ARecord"      = $fullresults.dns.a
-                    "AAAADomain"   = $fullresults.dns.aaaa
-                    "MXDomain"     = $fullresults.dns.mx
-                    "NSDomain"     = $fullresults.dns.ns
+        if ($PSCmdlet.ShouldProcess("Target", "Operation")) {
+            try {
+                foreach ($result in $fullresults) {
+                    $properties = @{
+                        "Domain"       = $fullresults.domain
+                        "WEB"          = $fullresults.web
+                        "DomainRecord" = $fullresults.dns.domain
+                        "ARecord"      = $fullresults.dns.a
+                        "AAAADomain"   = $fullresults.dns.aaaa
+                        "MXDomain"     = $fullresults.dns.mx
+                        "NSDomain"     = $fullresults.dns.ns
                     
+                    }
                 }
             }
-        }
-        finally {
-            $object = New-Object -TypeName PSObject -Property $properties
-            Write-Output $object
+            finally {
+                $object = New-Object -TypeName PSObject -Property $properties
+                Write-Output $object
+            }
         }
     }
-    
     end {
-        
     }
 }
