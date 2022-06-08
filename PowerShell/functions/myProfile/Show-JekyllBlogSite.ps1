fdca27532eb8014a2585a1717775ad8665d9e5f4
Luke Leigh <luke.leigh@gmail.com>
2022-06-08 20:48:41 +0100

    Update Show-JekyllBlogSite.ps1

--- PowerShell/functions/myProfile/Show-JekyllBlogSite.ps1
+++ PowerShell/functions/myProfile/Show-JekyllBlogSite.ps1
@@ -1,4 +1,4 @@
-function Show-JekyllBlogSite {
+function Show-LocalBlogSite {
 	$ComputerDNSName = $env:COMPUTERNAME + '.' + (Get-NetIPConfiguration).NetProfile.Name
 	$URL = "http://" + $ComputerDNSName + ":4000"
 	Start-Process $URL
