243bdfd6b36c7cf43ae560ab29283641f58a44d4
Luke Leigh <luke.leigh@gmail.com>
2025-09-20 05:02:10 +0100

    Remove FFMpegFunctions.ps1 after refactoring
    Deleted FFMpegFunctions.ps1 as its contents have been split into individual function files for better modularity and maintainability.

--- PowerShell/UserAdminModule/MediaManagement/Public/FFMpegFunctions.ps1
+++
@@ -1,5 +0,0 @@
-# This file has been separated into individual function files:
-# - Get-FFProbeVideoInfo.ps1
-# - Remove-FFMpegVideoFileAudioStream.ps1
-# - Set-FFMpegVideoSpeed.ps1
-# - Get-FFProbeAudioStreams.ps1
